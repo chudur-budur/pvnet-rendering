e943c44e89c9bf868484f2c28d735d4cf0a7d3f7
khaled <khaled@nod-labs.com>
2021-05-26 21:32:09 -0400

    logging

--- blender/render_utils.py
+++ blender/render_utils.py
@@ -260,7 +260,7 @@ class Renderer(object):
         count = 0 
         fp = open("render_utils.log", 'w')
         fp.write("Saving exr to png, lol\n")
-        fp.write("lmfao: " + str(depth_paths))
+        fp.write("lmfao: " + str(depth_paths) + '\n')
         for depth_path in depth_paths:
             fp.write("here, lol\n")
             self.exr_to_png(depth_path)
